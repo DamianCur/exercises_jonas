daaaf8e109dc927543f3119b9d4a33e361f72b33
xoriston <1420damian@gmail.com>
2022-05-10 20:44:42 +0200

    practicing async queue working (micro-tasks)

--- training/app.js
+++ training/app.js
@@ -1,7 +1,14 @@
-// const apiData = new XMLHttpRequest();
-// console.log(apiData);
-// apiData.open('GET', 'https://restcountries.com/v3.1/all');
+console.log('Test start');
 
-setInterval(() => {
-console.log("Nataluśka paruwuśka.");
-},100)
+setTimeout(() => {
+	console.log('0 sec timer');
+}, 0);
+
+Promise.resolve('Resolved promise 1').then((res) => console.log(res));
+
+Promise.resolve('Resolved promise 2').then((res) => {
+	for (let i = 0; i < 10000000000; i++) {}
+	console.log(res);
+});
+
+console.log('Test end');
