c851574b95ea34ffc27132d879d1737496b2db3b
xoriston <1420damian@gmail.com>
2022-05-12 20:46:03 +0200

    image async training

--- training/app.js
+++ training/app.js
@@ -1,28 +1,30 @@
-const lotteryPromise = new Promise((resolve, reject) => {
-	console.log('Lottery draw is happening ⏳');
+// const lotteryPromise = new Promise((resolve, reject) => {
+// 	console.log('Lottery draw is happening ⏳');
 
-	setTimeout(() => {
-		if (Math.random() >= 0.5) {
-			resolve('You win 🎁');
-		} else {
-			reject(new Error('You lost your money 😫'));
-		}
-	}, 2000);
-});
+// 	setTimeout(() => {
+// 		if (Math.random() >= 0.5) {
+// 			resolve('You win 🎁');
+// 		} else {
+// 			reject(new Error('You lost your money 😫'));
+// 		}
+// 	}, 2000);
+// });
 
-lotteryPromise.then((res) => console.log(res)).catch((err) => console.log(err));
+// lotteryPromise.then((res) => console.log(res)).catch((err) => console.log(err));
 
-const wait = (seconds) => {
-	return new Promise((resolve) => {
-		setTimeout(resolve, seconds * 1000);
-	}, seconds);
-};
+// const wait = (seconds) => {
+// 	return new Promise((resolve) => {
+// 		setTimeout(resolve, seconds * 1000);
+// 	}, seconds);
+// };
 
-wait(2)
-	.then(() => {
-		console.log('I waited for 2 seconds');
-		return wait(1);
-	})
-	.then(() => {
-		console.log('I waited for 1 second');
-	});
+// wait(2)
+// 	.then(() => {
+// 		console.log('I waited for 2 seconds');
+// 		return wait(1);
+// 	})
+// 	.then(() => {
+// 		console.log('I waited for 1 second');
+// 	});
+
+
