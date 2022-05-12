8fc837ac578e7f9ddaa9727511113a927c9c96dd
xoriston <1420damian@gmail.com>
2022-05-12 22:02:30 +0200

    practiciing promisses

--- training/app.js
+++ training/app.js
@@ -12,12 +12,6 @@
 
 // lotteryPromise.then((res) => console.log(res)).catch((err) => console.log(err));
 
-// const wait = (seconds) => {
-// 	return new Promise((resolve) => {
-// 		setTimeout(resolve, seconds * 1000);
-// 	}, seconds);
-// };
-
 // wait(2)
 // 	.then(() => {
 // 		console.log('I waited for 2 seconds');
@@ -27,4 +21,68 @@
 // 		console.log('I waited for 1 second');
 // 	});
 
+/* 
+Build the image loading functionality that I just showed you on the screen.
 
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast. */
+
+const wait = (seconds) => {
+	return new Promise((resolve) => {
+		setTimeout(resolve, seconds * 1000);
+	}, seconds);
+};
+
+const imgContainer = document.querySelector('.images');
+
+const imgChange = (imgPath) => {
+	return new Promise((resolve, reject) => {
+		const img = document.createElement('img');
+		img.src = imgPath;
+
+		img.addEventListener('load', () => {
+			imgContainer.append(img);
+			resolve(img);
+		});
+
+		img.addEventListener('error', () => {
+			reject(new Error('Cannot load image.😫'));
+		});
+	});
+};
+let currentImage;
+
+imgChange('./img/img-1.jpg')
+	.then((img) => {
+		console.log('Image one loaded ');
+		currentImage = img;
+		return wait(2);
+	})
+	.then(() => {
+		currentImage.style.display = 'none';
+		return imgChange('./img/img-2.jpg');
+	})
+	.then((img) => {
+		console.log('Image two loaded ');
+		currentImage = img;
+		return wait(2);
+	})
+	.then(() => {
+        currentImage.style.display = 'none';
+    })
+	.catch((err) => {
+		console.error(err);
+	});
