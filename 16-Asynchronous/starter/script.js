3bd58a84c649029d0a7006b58d5b6bcafaf07a92
xoriston <1420damian@gmail.com>
2022-05-18 20:43:39 +0200

    training race combinator

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -115,17 +115,27 @@ const getPosition = () => {
 // })();
 // console.log([data1.capital, data2.capital, data3.capital]);
 
-const get3Counteries = async (c1, c2, c3) => {
-  try {
-    const data = await Promise.all([
-      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
-    ]);
-    console.log(data);
-  } catch (err) {
-    console.error(err.message);
-  }
-};
+// const get3Counteries = async (c1, c2, c3) => {
+//   try {
+//     const data = await Promise.all([
+//       getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+//     ]);
+//     console.log(
+//       data.map(country => {
+//         return country[0].capital;
+//       })
+//     );
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// };
 
-get3Counteries('portugal', 'canada', 'poland');
+// get3Counteries('portugal', 'canada', 'poland');
+
+
+(async () => {
+  const res = await Promise.race([getJSON(`https://restcountries.com/v3.1/name/italy`), getJSON(`https://restcountries.com/v3.1/name/poland`), getJSON(`https://restcountries.com/v3.1/name/ukraine`)])
+  console.log(res[0]);
+})()
